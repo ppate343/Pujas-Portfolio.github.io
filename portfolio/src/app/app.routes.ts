3b3dc064c34ea874e1e264451b26de1cd41135d9
ppate343 <ppate343@uwo.ca>
2024-01-16 17:37:58 -0500

    removed route ref

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [ 
-{ path: '', redirectTo: '/nav-bar', pathMatch: 'full' },
-{ path: 'nav-bar', component: NavBarComponent }];
+// { path: '', redirectTo: '/nav-bar', pathMatch: 'full' },
+// { path: 'nav-bar', component: NavBarComponent },
+// { path: '', redirectTo: '/about', pathMatch: 'full' },
+// { path: 'about', component: AboutComponent }
+];
