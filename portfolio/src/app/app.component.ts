54ca6d2ebc078a52ab26814b9b6d07b073d1de87
ppate343 <ppate343@uwo.ca>
2024-01-16 17:38:12 -0500

    imported component

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavBarComponent, AboutComponent],
   templateUrl: './app.component.html',
-  styleUrls: [],
+  styleUrls: ['../styles.css'],
 })
 export class AppComponent {
   title = 'portfolio';
